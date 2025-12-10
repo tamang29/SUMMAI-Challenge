55d2a96d33538bad82820811af0b40b09828f561
tamang29 <tamangravi01@gmail.com>
2025-12-10 13:29:51 +0100

    Initial setup: server, webapp and bpmn-js

---
+++ webapp/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import ModelEditor from './components/ModelEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SUMMAI Challenge</h1>
+      <p>BPMN Real-Time Collaboration Platform</p>
+      <ModelEditor />
+    </div>
+  );
+}
+
+export default App;
